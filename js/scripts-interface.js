bb2fdeb148c8d5501528347fe592c7ce06fea101
David Rolfs <Davidrolfs15@gmail.com>
2017-06-30 08:28:05 -0700

    logs array in console

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,1 +1,16 @@
+var Doctors = require('./../js/scripts.js').doctorsModule;
+var apiKey = require('./../.env').apiKey;
 
+$(document).ready(function(){
+  $("#form").submit(function(event){
+    event.preventDefault();
+
+    newDoctors = new Doctors();
+    var userInput = $("#userInput").val();
+
+    newDoctors.getDoctors(userInput, apiKey);
+
+
+
+  });
+});

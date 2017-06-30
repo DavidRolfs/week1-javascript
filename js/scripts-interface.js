42a2bfd5670a380ba1dfacff455a8eafa98b1365
David Rolfs <Davidrolfs15@gmail.com>
2017-06-30 08:57:22 -0700

    displays first name of doctors in a list

--- js/scripts-interface.js
+++ js/scripts-interface.js
@@ -1,6 +1,12 @@
 var Doctors = require('./../js/scripts.js').doctorsModule;
 var apiKey = require('./../.env').apiKey;
 
+var display = function(profile) {
+  profile.forEach(function(firstName){
+    $('#output').append("<li>" + firstName + "</li>");
+  });
+};
+
 $(document).ready(function(){
   $("#form").submit(function(event){
     event.preventDefault();
@@ -8,9 +14,6 @@ $(document).ready(function(){
     newDoctors = new Doctors();
     var userInput = $("#userInput").val();
 
-    newDoctors.getDoctors(userInput, apiKey);
-
-
-
+    newDoctors.getDoctors(userInput, apiKey, display);
   });
 });
--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,15 @@
 Doctors = function(){
 };
 
-Doctors.prototype.getDoctors = function(medicalIssue, apiKey) {
+Doctors.prototype.getDoctors = function(medicalIssue, apiKey, display) {
   var array = []
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' +  medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-     result.data.forEach(function(element){
-       array.push(element.profile)
-       //console.log(element.profile);
+     var doctorProfile = result.data
+     doctorProfile.forEach(function(element){
+       array.push(element.profile.first_name)
      });
+     display(array)
      console.log(array)
     })
    .fail(function(error){

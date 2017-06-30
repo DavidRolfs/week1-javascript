1a10d2d01bd64d384bda3ea3edee3d59ec400d99
David Rolfs <Davidrolfs15@gmail.com>
2017-06-30 08:43:31 -0700

    gets doctors by profile and stores them in an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 Doctors = function(){
 };
 
-
 Doctors.prototype.getDoctors = function(medicalIssue, apiKey) {
+  var array = []
   $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' +  medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
    .then(function(result) {
-      console.log(result);
+     result.data.forEach(function(element){
+       array.push(element.profile)
+       //console.log(element.profile);
+     });
+     console.log(array)
     })
    .fail(function(error){
       console.log("fail");
